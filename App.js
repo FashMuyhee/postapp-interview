33f3482ce495784483795b9a3ce8cb2abbd83e31
Fash Muyhee <fashmuyidavid@gmail.com>
2024-03-12 18:02:56 +0100

    add persist functionality

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import RootNavigator from './src/navigation';
-import { store } from './src/redux';
+import { persistor, store } from './src/redux';
 import { Provider } from 'react-redux';
+import { RootSiblingParent } from 'react-native-root-siblings';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 export default function App() {
   return (
     <Provider store={store}>
-      <RootNavigator />
+      <PersistGate persistor={persistor} loading={null}>
+        <RootSiblingParent>
+          <RootNavigator />
+        </RootSiblingParent>
+      </PersistGate>
     </Provider>
   );
 }
